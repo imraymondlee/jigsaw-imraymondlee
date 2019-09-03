2e00a9f92060c8a944f0da9e5b9ba7f642704ba5
imraymondlee <imleeraymond@gmail.com>
2019-09-03 16:59:12 -0700

    Add header and footer partials

---
+++ source/_partials/header.blade.php
@@ -0,0 +1,11 @@
+<header>
+  <nav class="nav">
+    <div class="nav__left">
+      <a href="index.html"><img src="/assets/images/logo.svg" alt="Raymond Lee" class="nav__logo"></a>
+    </div>
+    <ul class="nav__list">
+      <li class="nav__item"><a href="#projects" class="nav__link">Projects</a></li>
+      <li class="nav__item"><a href="about.html" class="nav__link">About</a></li>
+    </ul>
+  </nav>
+</header>
