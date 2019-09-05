29e05ecd37493eb404d338a49e2f1d94dbf29221
imraymondlee <imleeraymond@gmail.com>
2019-09-04 20:41:18 -0700

    Add Favicon and Google Font

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NR3FFJW');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>{{ $title }}</title>
     <link rel="stylesheet" href="/assets/build/css/normalize.css">
     <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
-    <link rel="shortcut icon" type="image/x-icon" href="/assets/images/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
   </head>
   <body>
     @include('_partials.header')
