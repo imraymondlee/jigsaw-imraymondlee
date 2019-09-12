8f0de76702153e2c39b6489e15dbc672fb15b21e
imraymondlee <imleeraymond@gmail.com>
2019-09-12 15:56:50 -0700

    Add Resume

--- source/about/index.blade.php
+++ source/about/index.blade.php
@@ -5,7 +5,7 @@
     <img src="/assets/images/profile.jpg" class="about__img" alt="Self-portrait">
     <div class="about__text">
       <h1>Hello 👋</h1>
-      <p>I'm a web developer based in Vancouver, Canada. Currently, I am finishing up my Bachelor of Science degree and Business Minor at Simon Fraser University. My experience includes working in <strong>web development</strong>, <strong>design</strong>, <strong>accessibility</strong>, and <strong>marketing</strong>. With my passion for both the web and digital accessibility, my goal is to make the web a more accessible place. In my free time, you can find me learning and playing with new technology.</p>
+      <p>I’m Raymond Lee, a web developer from Vancouver, Canada. Graduating from Simon Fraser University with my Bachelor of Science and Business Minor, I have a diverse skill set that I hope to use in projects. My experience includes working in <strong>web development</strong>, <strong>design</strong>, <strong>accessibility</strong>, and <strong>marketing</strong>. My goal and passion is to make the web a more accessible place through my work. In my free time, you can find me learning and playing with new technology.</p>
       <p>Feel free to reach out to me at <a href="mailto:hello@imraymondlee.com">hello@imraymondlee.com</a>.</p>
     </div>
   </section>
