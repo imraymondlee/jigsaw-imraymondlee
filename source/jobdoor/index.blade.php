c096e053080e82dd8a313714dbbc9090e0b66ebc
imraymondlee <imleeraymond@gmail.com>
2019-12-27 20:13:10 -0800

    Add images to Jobdoor page

--- source/jobdoor/index.blade.php
+++ source/jobdoor/index.blade.php
@@ -1,4 +1,4 @@
-@extends('_layouts.master', ['title'=>'Our Cookbook | Raymond Lee'])
+@extends('_layouts.master', ['title'=>'Jobdoor | Raymond Lee'])
 
 @section('content')
   @include('_components.project-banner', [
@@ -21,23 +21,23 @@
   <hr class="divider">
   <section>
     <h2>Overview</h2>
-    <p>Jobdoor is a job board website for interns and new grads. The inspiration for this project was from a GitHub repo that contained a list of new grad positions that many people have contributed to. This project was built using Angular, Node.js, and MongoDB.</p>
+    <p>Jobdoor is a job board website for interns and new grads. Users can look for job opportunities or post ones that they have found. The inspiration for this project was from a GitHub repo that contained a list of new grad positions that many people have contributed to. This project was built using Angular, Node.js, and MongoDB.</p>
     <h2>Process</h2>
     <h3 class="u-h4">Back-end</h3>
     <p>The back-end was built with Node.js and data was stored with MongoDB. Endpoints were created to manage the postings and user accounts through different types of HTTP methods. The users are stored in one MongoDB collection and the postings are in another.</p>
     <h3 class="u-h4">Front-end</h3>
     <p>The front-end was built using Angular to provide quick loading between pages as it is a single-page application. Although most of the styles were custom made, I had an issue with creating a dialog, so I ended up using the dialog component from Angular Material UI. For the design, I went with a fun website personality through the use of bright colours, curves, and icons.</p>
     @include('_components.figure-caption', [
-      'image'=>['url'=>'/assets/images/our-cookbook/recipe.png', 'alt'=>'Recipe page for Slow Cooker Beef Chili'],
-      'caption'=>'The minimal interface of each recipe allows users to see essential information.'
+      'image'=>['url'=>'/assets/images/jobdoor/register.png', 'alt'=>'Registration page'],
+      'caption'=>'Registration is required to create job postings.'
     ])
     @include('_components.figure-caption', [
-      'image'=>['url'=>'/assets/images/our-cookbook/new-recipe.png', 'alt'=>'Page to add a new recipe'],
-      'caption'=>'New Recipe page.'
+      'image'=>['url'=>'/assets/images/jobdoor/post-a-job.png', 'alt'=>'Post a Job page'],
+      'caption'=>'It is quick to create a job posting with just 4 fields.'
     ])
     @include('_components.figure-caption', [
-      'image'=>['url'=>'/assets/images/our-cookbook/mobile-homepage.png', 'alt'=>'Homepage in mobile view'],
-      'caption'=>'Responsive interface allows the web app to be accessed from different type of devices.',
+      'image'=>['url'=>'/assets/images/jobdoor/mobile-homepage.png', 'alt'=>'Homepage in mobile view'],
+      'caption'=>'Responsive interface allows the website to be accessed from different type of devices.',
       'useOriginalImgWidth'=>true
     ])
     <h2>Reflection</h2>
